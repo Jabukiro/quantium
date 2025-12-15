13d4e57ad7c4185249dee57dd786a087a5df9c5f
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2025-12-15 02:26:17 +0200

    Data Processed

---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import csv
+FILENAMES = ["data/daily_sales_data_0.csv","data/daily_sales_data_1.csv", "data/daily_sales_data_2.csv"]
+NEWFILENAME = "data/processed_data"
+
+def filter_pink_morsels(productName: str):
+    return productName == "pink morsel"
+def calc_sales(price, quantity):
+    return float(price[1:-1]) * int(quantity)
+
+
+
+
+def main():
+    processedData=[["product","sales","date","region"]]
+
+    for filename in FILENAMES:
+        with open(filename, newline='') as csvfile:
+            filereader = csv.DictReader(csvfile)
+            for row in filereader:
+                if filter_pink_morsels(row["product"]):
+                    sales = calc_sales(row["price"], row["quantity"])
+                    processedData.append([','.join([row['product'], str(sales), row['date'], row['region']])])
+
+    with open(NEWFILENAME, mode="w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(processedData)
+
+main()
